4181aa5fd6da11e7fc9c195d11ecc6f47b892dad
Sanjeev Madhav <sduddela@tataunistore.com>
2022-07-19 16:06:58 +0530

    add swipe refresh layout

--- app/src/main/java/com/example/layouts/MainActivity.kt
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -2,46 +2,44 @@ package com.example.layouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.util.Log
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
-//    private val weekDaysList = listOf<String>(
-//        "Monday",
-//        "Tuesday",
-//        "Wednesday",
-//        "Thursday",
-//        "Friday",
-//        "Saturday",
-//        "Sunday",
-//    )
+    private val weekDaysList = arrayListOf<String>(
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    )
 
-    private val users = mutableListOf<User>()
-//    private lateinit var adapter: ArrayAdapter<User>
+//    private val users = mutableListOf<User>()
+    private lateinit var adapter: ArrayAdapter<String>
 //    lateinit var spWeekDays: Spinner
-    private lateinit var userAdapter: UserAdapter
-    private lateinit var recyclerView: RecyclerView
+//    private lateinit var userAdapter: UserAdapter
+    private lateinit var lvUsers: ListView
+//    private lateinit var recyclerView: RecyclerView
+    private lateinit var refreshLayout: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.listview)
 
-        for (i in 0..10) {
-            users.add(User(i, "Person $i", "someone$i@gmail.com"))
-        }
+//        for (i in 0..10) {
+//            users.add(User(i, "Person $i", "someone$i@gmail.com"))
+//        }
 
-//        adapter = ArrayAdapter<User>(
-//            this,
-//            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-//            users,
-//        )
+        adapter = ArrayAdapter<String>(
+            this,
+            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+            weekDaysList,
+        )
 //        spWeekDays = findViewById(R.id.spWeekdays)
 //        spWeekDays.adapter = adapter
 //
@@ -54,12 +52,25 @@ class MainActivity : AppCompatActivity() {
 //            }
 //
 //        }
-        recyclerView = findViewById(R.id.rvUsers)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        userAdapter = UserAdapter(users) {
-            Toast.makeText(this, users[it].email, Toast.LENGTH_SHORT).show()
+//        recyclerView = findViewById(R.id.rvUsers)
+//        recyclerView.setHasFixedSize(true)
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        userAdapter = UserAdapter(users) {
+//            Toast.makeText(this, users[it].email, Toast.LENGTH_SHORT).show()
+//        }
+//        recyclerView.adapter = userAdapter
+        lvUsers = findViewById(R.id.lvUsers)
+        lvUsers.adapter = adapter
+        lvUsers.setOnItemClickListener { _, _, index, _ ->
+            Toast.makeText(this, weekDaysList[index], Toast.LENGTH_SHORT).show()
         }
-        recyclerView.adapter = userAdapter
+
+        refreshLayout = findViewById<SwipeRefreshLayout>(R.id.refreshLayout).apply {
+            setOnRefreshListener {
+                weekDaysList.add(0,weekDaysList.random())
+                adapter.notifyDataSetChanged()
+                this.isRefreshing = false
+            }
+        }
     }
 }
